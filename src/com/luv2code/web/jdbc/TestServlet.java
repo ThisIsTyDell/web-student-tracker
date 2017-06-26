9c222c2f1d71ffce22ea70bd6d1d2cc221aac349
ThisIsTyDell <milleb94@gmail.com>
2017-06-26 16:46:30 -0500

    establish database connection

--- src/com/luv2code/web/jdbc/TestServlet.java
+++ src/com/luv2code/web/jdbc/TestServlet.java
@@ -2,6 +2,9 @@ package com.luv2code.web.jdbc;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -29,6 +32,18 @@ public class TestServlet extends HttpServlet {
 		// Step 1: Set up the printwriter
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/plain");
+		
+		// Step 2: Get a connection to the database
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = dataSource.getConnection();
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 		
 	}
 
