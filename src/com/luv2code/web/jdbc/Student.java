ad574f93fcd5cd54f93165d3f9f1ee8b1e58905e
ThisIsTyDell <milleb94@gmail.com>
2017-06-28 10:27:46 -0500

    add constructors for Student class

--- src/com/luv2code/web/jdbc/Student.java
+++ src/com/luv2code/web/jdbc/Student.java
@@ -6,5 +6,14 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private String email;
-
+	
+	public Student(int id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
 }
