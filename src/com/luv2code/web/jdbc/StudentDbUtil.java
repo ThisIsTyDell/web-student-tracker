6b383f0c6df85a82e18a6f54ca667e71af644259
ThisIsTyDell <milleb94@gmail.com>
2017-06-28 15:18:07 -0500

    remove unused import

--- src/com/luv2code/web/jdbc/StudentDbUtil.java
+++ src/com/luv2code/web/jdbc/StudentDbUtil.java
@@ -3,7 +3,6 @@ package com.luv2code.web.jdbc;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
