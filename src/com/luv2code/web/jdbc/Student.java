0e6d4db5ad041bf76b09f97944585b0f987a5b26
ThisIsTyDell <milleb94@gmail.com>
2017-06-28 10:30:02 -0500

    add additional constructor without id

--- src/com/luv2code/web/jdbc/Student.java
+++ src/com/luv2code/web/jdbc/Student.java
@@ -7,12 +7,17 @@ public class Student {
 	private String lastName;
 	private String email;
 	
+	public Student(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
 	public Student(int id, String firstName, String lastName, String email) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
-	
 	
 }
